60c0d35e6a452704f1772c200bb30e31f599ff5e
leandrosouzaa <leandror1.souza@hotmail.com>
2020-01-05 18:53:04 -0300

    Criação da API e da Primeira Rota

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const server = express();
+
+//o usuário pode enviar route params e os query params
+//o route vem direto e o query vem com o '?' antes
+//o req também pode enviar o body (para o get e input)
+//o res é a resposta da requisição
+
+server.get('/teste', (req, res) => {
+  console.log('teste');
+  return res.json({
+    message:'Hello World'
+  })
+})
+
+server.listen(3000)
