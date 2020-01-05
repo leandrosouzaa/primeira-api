4d06d16ce872f9bba9aa84c1625342eaee73dfd9
leandrosouzaa <leandror1.souza@hotmail.com>
2020-01-05 19:20:09 -0300

    Consumindo Query Params

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const server = express();
 
-//o usuário pode enviar route params e os query params
-//o route vem direto e o query vem com o '?' antes
+//o usuário pode enviar route params e os query params(users/1)
+//o route vem direto e o query vem com o '?' antes(?teste=1)
+// request body vem o corpo da requisição (dados para serem salvos por exemplo)
 //o req também pode enviar o body (para o get e input)
 //o res é a resposta da requisição
 
 server.get('/teste', (req, res) => {
+  const nome = req.query.nome
   return res.json({
-    message:'Hello World'
+    message: `Hello ${nome}`
   })
 })
 
